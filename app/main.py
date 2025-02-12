cb64b35af0d859621d6550f251a13a4d2e62dd4b
Karolpineda <karolpineda@hotmail.com>
2025-02-12 11:49:51 -0500

    feat: :sparkles: Update main

--- app/main.py
+++ app/main.py
@@ -1,10 +1,30 @@
-# app/main.py
 from fastapi import FastAPI
+from starlette.middleware.cors import CORSMiddleware 
+from app.controllers import user  
+from app.database import engine
 from app.models import Base
-from app.controllers.user import router as user_router
+from app.docs import swagger_router  
+
+app = FastAPI(
+    title="Mi API",
+    description="Documentatión of my API",
+    version="1.0.0",
+    docs_url=None,  
+    redoc_url=None  
+)
 
 
-app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  
+    allow_credentials=True,
+    allow_methods=["*"],  
+    allow_headers=["*"],  
+)
 
-# Incluir el router de usuario
-app.include_router(user_router)
+
+Base.metadata.create_all(bind=engine)
+
+
+app.include_router(user.router)
+app.include_router(swagger_router)
